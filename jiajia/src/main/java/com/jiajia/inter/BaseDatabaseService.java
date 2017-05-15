3f9f092fb7bfacf24f0aeda58f4d2b646fdc0028
liusijia <liusijia152503@163.com>
2017-05-15 14:55:34 +0800

    基础数据接口调整

--- jiajia/src/main/java/com/jiajia/inter/BaseDatabaseService.java
+++ jiajia/src/main/java/com/jiajia/inter/BaseDatabaseService.java
@@ -1,8 +1,35 @@
 package com.jiajia.inter;
 
+import java.util.List;
 import java.util.Map;
-
+/*
+ * 数据库基础服务类
+ * @auth liusijia
+ * 
+ */
 public interface BaseDatabaseService {
 	public void queryForMap(  Map<String,Object> map  );
 	public void updateAdmin( Object object );
+	
+	/**
+	 * 
+	 * 查询list集合
+	 */
+	public List queryForList();
+	/**
+	 * 
+	 * 新增对象
+	 */
+	public void insertObject(Object object);
+	/**
+	 * 
+	 * 修改对象
+	 */
+	public void updateObject(Object object);
+	/**
+	 * 
+	 * 删除对象
+	 * 根据主键id删除
+	 */
+	public void deleteObect(String id);
 }
